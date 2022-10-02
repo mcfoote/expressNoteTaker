d668cbc84bfedff3b8c8c9412472767241d72b94
Mitchell Foote <mcfoote321@gmail.com>
2022-10-02 18:41:01 -0400

    move routes to router file

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
+//import express
 const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./index.js');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(clog);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
+//Server hosts public folder
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes)
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
