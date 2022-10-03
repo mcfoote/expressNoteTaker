a56ad08f77b75eb9cc4ae3fc2705f18a118ef3c4
Mitchell Foote <mcfoote321@gmail.com>
2022-10-03 01:44:39 -0400

    fleshing out code

---
+++ db/dbScript.js
@@ -0,0 +1,22 @@
+const util = require('util');
+const fs = require('fs');
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.primisify(fs.readFile);
+
+class db {
+
+    addNote(data) {
+
+    }
+
+    deleteNote(data) {
+
+    }
+
+    read() {
+
+    }
+
+}
+
+module.exports = new db();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,28 @@
-//local variables for requirments
+//local variables for requirements
 const express = require('express');
 const router = express.Router();
-const db = require('../db/db.json');
+const db = require('../db/dbScript.json');
 const uuid = require('uuid');
 
+//get function
+router.get('/api/notes', function(req, res) {
+
+    const notes = await db.read();
+    return res.json(notes);
+
+});
+
+//post function
+router.post('/api/notes', function(req, res) {
+
+    const notesCurrent = await db.read();
+
+    let noteNew = {
+        id: uuid(),
+        title: req.body.title,
+        text: req.body.text
+    };
+
+    await db.addNote
+
+});
