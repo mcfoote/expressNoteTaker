f42fe9bd4f14f28d163ee3ab74dd5292fe806fd8
Mitchell Foote <mcfoote321@gmail.com>
2022-10-03 00:27:20 -0400

    fleshing out code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,6 @@
+//local variables for requirments
+const express = require('express');
+const router = express.Router();
+const db = require('../db/db.json');
+const uuid = require('uuid');
 
-const router = require('express').Router();
