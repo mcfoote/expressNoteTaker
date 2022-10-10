81f09d4037562fcbe83fd0b26bfe10940f0f9617
Mitchell Foote <mcfoote321@gmail.com>
2022-10-10 03:36:34 -0400

    continue rewrite

--- db/dbScript.js
+++ db/dbScript.js
@@ -2,12 +2,57 @@
 const path = require('path');
 const fs = require('fs');
 
-function addNote() {
+// function to create new notes
+function addNote(content, notesArr) {
+
+    const newNote = content;
+    notesArr.push(newNote);
+
+    fs.writeFileSync(
+        path.join(__dirname, './db.json'),
+        JSON.stringify({notesArr}, null, 2)
+    );
+
+    return newNote;
 
 }
 
-function deleteNote() {
+// function to delete targeted note
+function delNote(id, notes) {
+
+    let notesArr = notes.filter(el => {
+
+        if(el.id == id) {
+            return false
+        }else {
+            return true
+        }
+
+    })
+
+    //correct index
+    let index = 0;
+
+    notesArr.forEach(note => {
+        note.id = index;
+        index++;
+    });
+
+    //write out
+    fs.writeFileSync(
+        path.join(__dirname, './db.json'),
+        JSON.stringify({notesArr}, null, 2)
+    )
+
+    return notesArr;
 
 }
 
+
+module.exports = {
+
+    addNote,
+    delNote
+
+};
 
